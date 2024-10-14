82fc5ba2e063e72ecb70e73cd82703bf65cc857c
Ashu <ashutoshkhade2530@gmail.com>
2024-10-14 09:57:55 +0530

    complete rent calculator project

--- Rent_Calculator.py
+++ Rent_Calculator.py
@@ -1,13 +1,3 @@
-#input we need from the user 
-#Total rent
-#Total food orderd for snacking 
-#Electricity units spend 
-#Charge per unit 
-#persons living in room
-
-#Output
-#Total amount you have to pay is
-
 rent=int(input("Enter your hostel/flat rent="))
 food=int(input("Enter your amount of food ordered= "))
 electricity_spend=int(input("Enter your total Electricity spend amount= "))
